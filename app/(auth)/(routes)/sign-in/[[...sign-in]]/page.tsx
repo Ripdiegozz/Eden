4be054635336aaa03a71ec4f0ce8979c5a4c6c55
Diego Garcia <dagadev@proton.me>
2024-01-30 20:35:30 -0500

    Demo user

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,29 @@
 import { SignIn } from "@clerk/nextjs";
- 
+
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="flex flex-row flex-wrap gap-2 justify-center items-start pt-40 md:pt-0">
+      <SignIn />
+      {/* Demo user credentials */}
+      <div 
+        className="rounded-xl max-h-40 p-10 text-center text-sm text-gray-700 dark:text-gray-100 bg-white dark:bg-gray-800 border border-gray-500"
+      >
+        <p className="text-lg font-semibold">
+          Demo User credentials:
+        </p>
+        <p className="text-md">
+          Email:
+          <span className="font-bold pl-2">
+            demo@user.com
+          </span>
+        </p>
+        <p className="text-md">
+          Password:
+          <span className="font-bold pl-2">
+            demouser
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 }
